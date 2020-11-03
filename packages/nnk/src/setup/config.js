f58222b4d5268dc6c678c9cde68feeb03728a03f
Anna Branco <annya.branco@gmail.com>
2020-11-03 18:56:34 +0100

    add: [#8] added images & texts from HomePage

--- packages/nnk/src/setup/config.js
+++ packages/nnk/src/setup/config.js
@@ -2,10 +2,34 @@ import { LIGHT } from '../constants/theme';
 import { THEMES } from './themes';
 
 export default {
+  defaultLanguage: 'en',
   theme: LIGHT,
   mainPadding: '10px',
   headerPadding: '80px',
   buttonsBackground: THEMES[LIGHT].primary,
   buttonsText: THEMES[LIGHT].secondary,
-  buttonsHighlight: THEMES[LIGHT].terciary
+  buttonsHighlight: THEMES[LIGHT].terciary,
+  socialLinks: [
+    {
+      type: 'Facebook',
+      link: 'https://www.facebook.com/NoNameKitchenBelgrade'
+    },
+    {
+      type: 'Twitter',
+      link: 'https://www.twitter.com/nonamekitchen1'
+    },
+    {
+      type: 'Instagram',
+      link: 'https://www.instagram.com/no_name_kitchen/'
+    },
+    {
+      type: 'Youtube',
+      link: 'https://www.youtube.com/no_name_kitchen/'
+    },
+    {
+      type: 'Email',
+      link: 'mailto:nnk@nnk.org',
+      hide: true
+    }
+  ]
 };
