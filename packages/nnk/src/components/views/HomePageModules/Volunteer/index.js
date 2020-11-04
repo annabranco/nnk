709fce2940e6309337d68ff9717b58e960825797
Anna Branco <annya.branco@gmail.com>
2020-11-04 20:50:02 +0100

    feat: [#13} Created Be a Volunteer Module

--- packages/nnk/src/components/views/HomePageModules/Volunteer/index.js
+++ packages/nnk/src/components/views/HomePageModules/Volunteer/index.js
@@ -1,7 +1,33 @@
 import React from 'react'; // eslint-disable-line import/no-extraneous-dependencies
-import MainMockImage from '../../../../assets/images/mocks/mainPageMock02.jpg';
-import { MockImage } from './styles';
+import { VolunteersInAction } from '../../../../assets/images';
+import {
+  ModuleWrapper,
+  MainImage,
+  Title,
+  Highlighted,
+  ImgLink
+} from './styles';
 
-const VolunteerModule = () => <MockImage src={MainMockImage} />;
+const VolunteerModule = ({ texts, colors }) => {
+  const styleTitle = title => {
+    const words = title.split(' ');
+    const lastWord = words.pop();
+    return (
+      <Title colors={colors}>
+        {words.join(' ')}
+        <Highlighted colors={colors}> {lastWord}</Highlighted>?
+      </Title>
+    );
+  };
+
+  return (
+    <ModuleWrapper>
+      {styleTitle(texts.question)}
+      <ImgLink link="/players">
+        <MainImage src={VolunteersInAction} alt={texts.altMainImage} />
+      </ImgLink>
+    </ModuleWrapper>
+  );
+};
 
 export default VolunteerModule;
