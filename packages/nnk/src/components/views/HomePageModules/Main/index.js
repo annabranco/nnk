9b300bdece7a0825cfdd0d20c311ae310deea40c
Anna Branco <annya.branco@gmail.com>
2020-12-09 18:36:40 +0100

    feat: [#53, #78, #79] style enhancements from previous sprint

--- packages/nnk/src/components/views/HomePageModules/Main/index.js
+++ packages/nnk/src/components/views/HomePageModules/Main/index.js
@@ -1,8 +1,11 @@
-import React from 'react'; // eslint-disable-line import/no-extraneous-dependencies
-import { HomeBG } from '../../../../assets/images';
+import { connect } from 'frontity';
+import React, { useEffect } from 'react'; // eslint-disable-line import/no-extraneous-dependencies
+import { Home } from '../../../../assets/images';
+import { HOMEPAGE_TEXTS } from '../../../../db';
+import { StatePropType } from '../../../../types';
 import { getSocialLinks } from '../../../../utils';
 import SocialModule from '../../SocialContainer';
-import { PhotoContainer } from './styles';
+import { MainText, PhotoContainer } from './styles';
 
 const socialLinks = getSocialLinks([
   'Facebook',
@@ -12,10 +15,24 @@ const socialLinks = getSocialLinks([
   'Email'
 ]);
 
-const MainModule = () => (
-  <PhotoContainer img={HomeBG}>
-    <SocialModule socialLinks={socialLinks} />
-  </PhotoContainer>
-);
+const MainModule = ({ state }) => {
+  const { colors, language } = state.theme;
+  let homepageTexts = HOMEPAGE_TEXTS[language];
 
-export default MainModule;
+  useEffect(() => {
+    homepageTexts = HOMEPAGE_TEXTS[language];
+  }, [language]);
+
+  return (
+    <PhotoContainer img={Home}>
+      <MainText colors={colors}>{homepageTexts.mainText}</MainText>
+      <SocialModule socialLinks={socialLinks} />
+    </PhotoContainer>
+  );
+};
+
+MainModule.propTypes = {
+  state: StatePropType.isRequired
+};
+
+export default connect(MainModule);
--- packages/nnk/src/components/views/HomePageModules/Main/styles.js
+++ packages/nnk/src/components/views/HomePageModules/Main/styles.js
@@ -1,7 +1,33 @@
 import { styled, css } from 'frontity';
+import { BaseText, sizeLargeTitle } from '../../../../setup/themes';
+
+export const MainText = styled(BaseText)`
+  position: absolute;
+  bottom: 15vh;
+  right: 6vw;
+  white-space: pre;
+  font-size: ${sizeLargeTitle};
+  font-style: italic;
+  font-weight: 900;
+  line-height: 1.5;
+  /* text-shadow: 0 0 10px black; */
+  border-radius: 10px;
+  padding: 5px 10px;
+  -webkit-text-stroke: 1px black;
+  text-shadow: -2px -2px 5px black, -2px 0 5px black, -2px 2px 5px black,
+    0 -2px 5px black, 0 0 5px black, 0 2px 5px black, 2px -2px 5px black,
+    2px 0 5px black, 2px 2px 5px black;
+  letter-spacing: -0.15rem;
+`;
+MainText.displayName = 'MainText';
+// white-space forces line breaks \n
 
 export const PhotoContainer = styled.div`
   position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: flex-end;
   height: calc(100vh - 100px);
   width: 100%;
 
